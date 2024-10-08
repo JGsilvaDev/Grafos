4c53a1a4ae10179de31c8d0bbbcc8bcbd4a908c9
RodrigoDEV <rocapucho123123@gmail.com>
2024-10-08 10:40:34 -0300

    feat: sistema de pesquisa

---
+++ static/js/search-data.js
@@ -0,0 +1,42 @@
+export const DATA =
+    [
+        {
+            name: "Prédio Principal",
+            andares:
+                [
+                    {
+                        name: "Primeiro andar",
+                        salas:
+                            [
+                                { name: "SALA 1", label: "SALA_01" },
+                                { name: "SALA 2", label: "SALA_02" },
+                                { name: "SALA 3", label: "SALA_03" },
+                            ]
+                    },
+                    {
+                        name: "Segundo andar",
+                        salas:
+                            [
+                                { name: "SALA 1", label: "SALA_01" },
+                                { name: "SALA 2", label: "SALA_02" },
+                                { name: "SALA 3", label: "SALA_03" },
+                            ]
+                    },
+                ]
+        },
+        {
+            name: "Prédio Segundo",
+            andares:
+                [
+                    {
+                        name: "Primeiro andar",
+                        salas:
+                            [
+                                { name: "SALA 1", label: "SALA_01" },
+                                { name: "SALA 2", label: "SALA_02" },
+                                { name: "SALA 3", label: "SALA_03" },
+                            ]
+                    }
+                ]
+        },
+    ]
